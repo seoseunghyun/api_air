1d94c01c63f59d8570bc0219aeace567c14482c4
seunghyunnet <seunghyunnet@naver.com>
2022-03-27 19:45:41 +0900

    Update

--- index.js
+++ index.js
@@ -27,6 +27,15 @@ queryParams2 += '&' + encodeURIComponent('searchDate') + '=' + encodeURIComponen
 queryParams2 += '&' + encodeURIComponent('InformCode') + '=' + encodeURIComponent('PM10'); /* */
 
 
+
+var url3 = 'http://apis.data.go.kr/B552584/MsrstnInfoInqireSvc/getMsrstnList';
+var queryParams3 = '?' + encodeURIComponent('serviceKey') + '=6IG5R%2BT%2B0rr88bxlfAD9RMYHGV7RxrGRm1JdGXe8Bb%2BlncG%2Bz8B2hPiLerN3Kwx%2B5Y5tm2j5tfa3ZzWCfHEdLA%3D%3D'; /* Service Key*/
+queryParams3 += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); /* */
+queryParams3 += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('1000'); /* */
+queryParams3 += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
+queryParams3 += '&' + encodeURIComponent('addr') + '=' + encodeURIComponent(''); /* */
+queryParams3 += '&' + encodeURIComponent('stationName') + '=' + encodeURIComponent(''); /* */
+
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
       var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
@@ -35,37 +44,40 @@ function uuidv4() {
   }
 
 // schedule.scheduleJob("20 * * * *", function () {
+    
     request({
-        url: url + queryParams,
+        url: url3 + queryParams3,
         method: 'GET'
     }, function (error, response, body) {
-        //console.log('Status', response.statusCode);
-        //console.log('Headers', JSON.stringify(response.headers));
-        console.log('Reponse received', body);
-        // textwrite1.txt 파일에 동기적으로 쓰기
-        fs.writeFileSync('info_air.json', body, 'utf8');
-        fs.writeFileSync('update.txt', uuidv4(), 'utf8');
-        console.log('공기질 정보 성공');
-        console.log(__dirname);
+        var _body = JSON.parse(body);
+
+        fs.writeFileSync('info_center.json', body, 'utf8');
+        console.log('공기질 센터 정보 성공');
+
         request({
-            url: url2 + queryParams2,
+            url: url + queryParams,
             method: 'GET'
         }, function (error, response, body) {
-            console.log(url2 + queryParams2);
-            //console.log('Status', response.statusCode);
-            //console.log('Headers', JSON.stringify(response.headers));
-            console.log('Reponse received', body);
-            // textwrite1.txt 파일에 동기적으로 쓰기
-            fs.writeFileSync('info_air_forecast.json', body, 'utf8');
-            fs.writeFileSync('update.txt', uuidv4(), 'utf8');
-            console.log('공기질 예측 정보 성공');
-            console.log(__dirname);
-            if(shell.exec('./gitpush.sh').code !== 0)  {
-                shell.echo('error');
-                shell.exit(1);
-            } else {
-                console.log('done');
-            }
+            var _body = JSON.parse(body);
+    
+            fs.writeFileSync('info_air.json', body, 'utf8');
+            console.log('공기질 정보 성공');
+            // console.log(__dirname);
+            request({
+                url: url2 + queryParams2,
+                method: 'GET'
+            }, function (error, response, body) {
+                fs.writeFileSync('info_air_forecast.json', body, 'utf8');
+                fs.writeFileSync('update.txt', uuidv4(), 'utf8');
+                console.log('공기질 예측 정보 성공');
+                // console.log(__dirname);
+                if(shell.exec('./gitpush.sh').code !== 0)  {
+                    shell.echo('error');
+                    shell.exit(1);
+                } else {
+                    console.log('done');
+                }
+            });
         });
     });
 // });
