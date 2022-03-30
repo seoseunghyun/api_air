ed6b756aef5b420208e3eb5449685c3cf7b0f372
seoseunghyun <me@seunghyun.net>
2022-03-30 16:37:34 +0900

    Update

--- code/air.js
+++ code/air.js
@@ -3,4 +3,5 @@ document.addEventListener('deviceready', function(){
 }, false);
 
 
-document.getElementById("desc_donate").innerHTML = 'Welcome to Air<br />광고 보고 개발자 응원하기 (클릭)'
+document.getElementById("desc_donate").innerHTML = 'Welcome to Air<br />광고 보고 개발자 응원하기 (클릭)';
+// document.getElementById("btn_menu_today").innerHTML = '<i class="fa-solid fa-location-pin"></i>&nbsp;내 주변';
