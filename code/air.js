cb2e851249a7d47e2c171e9975cecc4a5dc11a0f
seoseunghyun <me@seunghyun.net>
2022-03-30 16:33:27 +0900

    Update

--- code/air.js
+++ code/air.js
@@ -1,3 +1,6 @@
 document.addEventListener('deviceready', function(){
     window.plugins.toast.showShortBottom('Beta v0.0.1');
 }, false);
+
+
+document.getElementById("desc_donate").innerHTML = 'Welcome to Air<br />광고 보고 개발자 응원하기 (클릭)'
