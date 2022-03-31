b45a7839e4819b51e7be9d4b6de530a540264f86
seoseunghyun <me@seunghyun.net>
2022-04-01 01:46:52 +0900

    Update

--- index.js
+++ index.js
@@ -85,16 +85,16 @@ function uuidv4() {
                 for(var i in _body.response.body.items) {
                     for(var j in _body.response.body.items[i]) {
                         var chkList = ['so2Grade', 'coFlag' ,'pm25Flag','pm10Flag', 'o3Grade', 'no2Flag', 'no2Grade', 'o3Grade', 'o3Flag', 'pm25Grade' ,'so2Flag', 'coGrade', 'pm10Grade'];
+                        if(_body.response.body.items[i][j] == null) {
+                            _body.response.body.items[i][j] = -1;
+                        }
                         if(chkList.indexOf(j) !== -1) {
                             // console.log("***")
                             delete _body.response.body.items[i][j];
                         }
-                        if(_body.response.body.items[i][j] != null && j == 'dataTime') {
+                        if(_body.response.body.items[i][j] != -1 && j == 'dataTime') {
                             console.log("-----", _body.response.body.items[i][j])
                             _body.response.body.items[i][j] = " " + _body.response.body.items[i][j].split(" ")[1];
-                        }
-                        if(_body.response.body.items[i][j] == null) {
-                            _body.response.body.items[i][j] = -1;
                         }
                     }
 
@@ -123,5 +123,5 @@ function uuidv4() {
             } else {
                 console.log('done');
             }
-        },5000)
+        },15000)
 // });
