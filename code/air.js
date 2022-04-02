6ac93e7c9e296876ed61a2c4c733d481354ad887
seoseunghyun <me@seunghyun.net>
2022-04-03 04:23:12 +0900

    update

--- code/air.js
+++ code/air.js
@@ -1,6 +1,6 @@
 document.addEventListener('deviceready', function(){
     // window.plugins.toast.showShortBottom('Beta v0.0.1');
-    $(".details_forecast_item").click(function(){
+    $(".details_forecast_item").on('touchstart',function(){
         window.alertMsg("예보",$(this).html())
     })
 }, false);
